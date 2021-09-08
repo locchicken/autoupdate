c57029e635427fbfc61e1bc72594a551e91603a1
Your Name <you@example.com>
2021-09-08 16:34:19 +0700

    first commit

---
+++ Update Data/Form1.Designer.cs
@@ -0,0 +1,166 @@
+﻿namespace Update_Data
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.versionindex = new System.Windows.Forms.Label();
+            this.versionserver = new System.Windows.Forms.Label();
+            this.status = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(10)))), ((int)(((byte)(3)))), ((int)(((byte)(9)))));
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Red;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(543, 0);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(32, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "X";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.label1.Location = new System.Drawing.Point(32, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(95, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Phiên bản hiện tại:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.label2.Location = new System.Drawing.Point(32, 75);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(113, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Phiên bản trên Server:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.label3.Location = new System.Drawing.Point(32, 111);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(58, 13);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Trạng thái:";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.ForeColor = System.Drawing.Color.Lime;
+            this.progressBar1.Location = new System.Drawing.Point(35, 146);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(498, 23);
+            this.progressBar1.TabIndex = 2;
+            // 
+            // versionindex
+            // 
+            this.versionindex.AutoSize = true;
+            this.versionindex.ForeColor = System.Drawing.Color.Yellow;
+            this.versionindex.Location = new System.Drawing.Point(151, 41);
+            this.versionindex.Name = "versionindex";
+            this.versionindex.Size = new System.Drawing.Size(31, 13);
+            this.versionindex.TabIndex = 1;
+            this.versionindex.Text = "0.0.1";
+            // 
+            // versionserver
+            // 
+            this.versionserver.AutoSize = true;
+            this.versionserver.ForeColor = System.Drawing.Color.Yellow;
+            this.versionserver.Location = new System.Drawing.Point(151, 75);
+            this.versionserver.Name = "versionserver";
+            this.versionserver.Size = new System.Drawing.Size(161, 13);
+            this.versionserver.TabIndex = 1;
+            this.versionserver.Text = "Chờ hệ thống kiểm tra phiên bản";
+            // 
+            // status
+            // 
+            this.status.AutoSize = true;
+            this.status.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.status.Location = new System.Drawing.Point(151, 111);
+            this.status.Name = "status";
+            this.status.Size = new System.Drawing.Size(126, 13);
+            this.status.TabIndex = 1;
+            this.status.Text = "Chờ hệ thống kiểm tra.....";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(10)))), ((int)(((byte)(3)))), ((int)(((byte)(9)))));
+            this.ClientSize = new System.Drawing.Size(574, 201);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.status);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.versionserver);
+            this.Controls.Add(this.versionindex);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseUp);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label versionindex;
+        private System.Windows.Forms.Label versionserver;
+        private System.Windows.Forms.Label status;
+    }
+}
+
---
+++ Update Data/Form1.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Threading;
+using System.IO;
+using System.IO.Compression;
+
+namespace Update_Data
+{
+    public partial class Form1 : Form
+    {
+        private WebClient web = new WebClient();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        // Trước tiên khai báo biến nè 
+        private bool dragging;
+        private Point pointClicked;
+        // Bắt sự kiện MouseUp cho nó nè 
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                dragging = true;
+                pointClicked = new Point(e.X, e.Y);
+            }
+            else
+            {
+                dragging = false;
+            }
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (dragging)
+            {
+                Point pointMoveTo;
+                pointMoveTo = this.PointToScreen(new Point(e.X, e.Y));
+                pointMoveTo.Offset(-pointClicked.X, -pointClicked.Y);
+                this.Location = pointMoveTo;
+            }
+        }
+
+        private void Form1_MouseUp(object sender, MouseEventArgs e)
+        {
+            dragging = false;
+        }
+
+       
+        private int type;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           
+            
+           
+            web.DownloadFileCompleted += Client_DownloadFileCompleted;
+            new Thread(delegate ()
+            {
+                Uri uri = new Uri(linkversion);
+                Path.GetFileName(uri.AbsolutePath);
+                this.web.DownloadFileAsync(uri, linkaddversionserver);
+            }).Start();
+            versionindex.Text = File.ReadAllText("update\\version.txt");
+
+        }
+
+        private void Client_DownloadProgressChanged(object sender, DownloadProgressChangedEventArgs e)
+        {
+            if (type == 0)
+            {
+                base.Invoke(new MethodInvoker(delegate ()
+                {
+                    double num = double.Parse(e.BytesReceived.ToString());
+                    double num2 = double.Parse(e.TotalBytesToReceive.ToString());
+                    double num3 = num / num2 * 100.0;
+                    status.Text = string.Format("{0:0.##}", num3) + "%";
+                    progressBar1.Value = int.Parse(Math.Truncate(num3).ToString());
+                }));
+                Thread.Sleep(1000);
+                MessageBox.Show("Đã cập nhập xong!");
+                return;
+            }
+           
+        }
+       void updata()
+        {
+            try
+            {
+                Thread.Sleep(5000);
+                File.Delete("Library");
+                web.DownloadFile("http://locdz.tk/Library.zip", @"Library.zip");
+                string path = @".\Library.zip";
+                string ex = @".\";
+                ZipFile.ExtractToDirectory(path, ex);
+                File.Delete(@".\Library.zip");
+                this.Close();
+            }
+            catch (Exception)
+            {
+
+            }
+        }
+        private void Client_DownloadFileCompleted(object sender, AsyncCompletedEventArgs e)
+        {
+            string s = File.ReadAllText(linkaddversion);
+            
+            try
+            {
+                if (type == 0)
+                {
+                    while (!File.Exists(linkaddversion))
+                    {
+                        Thread.Sleep(100);
+                    }
+                   
+                    versionserver.Text= File.ReadAllText(linkaddversionserver);//lấy phiên bản server gán vào text
+                    if (versionserver.Text != versionindex.Text)//nếu mà cái phiên bản trên sv khác vs phien bản hiện tại
+                    {
+                         DialogResult dia = MessageBox.Show("Đã có phiên bản mới bạn có muốn cập nhập không!", "Thông báo", MessageBoxButtons.YesNo);
+                       if(dia==DialogResult.Yes)
+                        {
+                             
+                            new Thread(delegate ()
+                            {
+                                Uri uri = new Uri(linkversion);
+                                Path.GetFileName(uri.AbsolutePath);
+                                web.DownloadFileAsync(uri, linkaddversion);
+                                updata();
+                            }).Start();
+                            web.DownloadProgressChanged += Client_DownloadProgressChanged;
+                            versionindex.Text = File.ReadAllText(linkaddversionserver);
+                        }
+                        if (dia == DialogResult.No)
+                        {
+                            status.Text = "Đã có phiên bản mới!";
+                           
+                            return;
+                        }
+                       
+                    }
+                    Thread.Sleep(3000);
+                    status.Text = "Chưa có phiên bản mới!";
+                    return;
+                }
+              
+            }
+            catch (Exception)
+            {
+
+            }
+        }
+        private string linkversion = "https://drive.google.com/u/0/uc?id=1AxqxTvB_P8fUf1-MviLpBgLLXs9dy4Xw&export=download";
+        private string linkaddversion = Application.StartupPath + "\\update\\version.txt";
+        private string linkaddversionserver= Application.StartupPath + "\\update\\server.txt";
+    }
+}
